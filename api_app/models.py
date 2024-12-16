f6d3cc2ab08d47d4bb3e79c2e44b1a6694391cc2
Dmitriy Alexandrov <newsmobileads@gmail.com>
2024-12-16 22:25:09 +0500

    Картинки для проектов

--- api_app/models.py
+++ api_app/models.py
@@ -6,23 +6,8 @@ from transliterate import translit
 from pytils.translit import slugify
 
 
-class Event(models.Model):
-    name = models.CharField(max_length=255, null=True, blank=True, unique=True)
-    date = models.DateField(null=True, blank=True)
-    time = models.TimeField(null=True, blank=True)
-    place = models.CharField(max_length=255, null=True, blank=True)
-    description = models.CharField(max_length=1000, null=True, blank=True)
-
-    def __str__(self):
-        return self.name[:30]
-
-    class Meta:
-        verbose_name = 'События'
-        verbose_name_plural = 'События'
-
-
 class News(models.Model):
-    title = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True, unique=True)
     text = models.TextField(null=True, blank=True)
     date_created = models.DateField(auto_now=True, null=True, blank=True)
     image = models.ImageField(null=True, blank=True, upload_to='news/')
@@ -45,11 +30,10 @@ class News(models.Model):
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.CharField(max_length=1000)
     link = models.URLField(null=True, blank=True)
-
-    # image ...
+    image = models.ImageField(null=True, blank=True, upload_to='projects/')
 
     def __str__(self):
         return self.name[:30]
@@ -57,6 +41,15 @@ class Project(models.Model):
     class Meta:
         verbose_name = 'Проекты'
         verbose_name_plural = 'Проекты'
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            name, extension = os.path.splitext(self.image.name)
+
+            title = slugify(self.name)
+            self.image.name = title + extension
+
+        super(Project, self).save(*args, **kwargs)
 
 
 class Apply(models.Model):
@@ -67,3 +60,18 @@ class Apply(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    place = models.CharField(max_length=255, null=True, blank=True)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name[:30]
+
+    class Meta:
+        verbose_name = 'События'
+        verbose_name_plural = 'События'
