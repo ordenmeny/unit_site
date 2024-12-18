c06de428fa27a6e43ad3ab916df52a9fd8f9bb7d
Dmitriy Alexandrov <newsmobileads@gmail.com>
2024-12-18 22:09:58 +0500

    settings.py hosts 2

--- unit_site/settings.py
+++ unit_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-*gph%0jjb8fwnf@5_!tca5=^uf%rv%zz-0r#&(%b-d!4=uy_#6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["unit-clubs.ru"]
 
 # Application definition
 
