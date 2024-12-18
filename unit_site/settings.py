9f8818eb5aa09a0d5142d269c1b6d0991171bed1
Dmitriy Alexandrov <newsmobileads@gmail.com>
2024-12-18 21:46:13 +0500

    settings.py hosts

--- unit_site/settings.py
+++ unit_site/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*gph%0jjb8fwnf@5_!tca5=^uf%rv%zz-0r#&(%b-d!4=uy_#6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
